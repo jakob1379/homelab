a3d5263e0a034e2395a939ef6b9482c5d451bd95
Jakob Guldberg Aaes <jakob1379@gmail.com>
2024-11-27 10:56:57 +0100

    lint

--- flake.nix
+++ flake.nix
@@ -1,19 +1,11 @@
 {
-  inputs = {
-    utils.url = "github:numtide/flake-utils";
-  };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          pre-commit
-          gitleaks
-          yamllint
-        ];
-      };
-    }
-  );
+  inputs = { utils.url = "github:numtide/flake-utils"; };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ pre-commit gitleaks yamllint ];
+        };
+      });
 }
